3be1a292337d272193b658b367af76a9a9905170
Josifs Makarenko <dimandboy@mail.ru>
2025-01-18 21:39:08 +0200

    Hot fix

--- src/main/java/lv/restaurant/WebRestaurantApplication.java
+++ src/main/java/lv/restaurant/WebRestaurantApplication.java
@@ -1,16 +1,12 @@
 package lv.restaurant;
 
-import lv.restaurant.web_ui.config.SpringWebConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+@SpringBootApplication
 public class WebRestaurantApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SpringWebConfiguration.class);
+        SpringApplication.run(WebRestaurantApplication.class, args);
     }
-
 }
--- src/main/java/lv/restaurant/config/SpringCoreConfiguration.java
+++ src/main/java/lv/restaurant/config/SpringCoreConfiguration.java
@@ -2,46 +2,33 @@ package lv.restaurant.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = "lv.javaguru.java2.wasterestarant.core")
-@PropertySource(value = "classpath:application.properties")
 @EnableTransactionManagement
 public class SpringCoreConfiguration {
-
-    @Value("${jdbc.url}")
-    private String jdbcUrl;
-
-    @Value("${driverClass}")
-    private String driverClass;
-
-    @Value("${database.user.name}")
-    private String userName;
-
-    @Value("${database.user.password}")
-    private String userPassword;
 
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl(jdbcUrl);
-        dataSource.setDriverClassName(driverClass);
-        dataSource.setUsername(userName);
-        dataSource.setPassword(userPassword);
+        dataSource.setUrl("jdbc:h2:mem:zerowaste;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("password");
         return dataSource;
     }
 
@@ -51,33 +38,28 @@ public class SpringCoreConfiguration {
     }
 
     @Bean
-    public Properties hibernateProperties(
-            @Value("${hibernate.show_sql}") Boolean showSql,
-            @Value("${hibernate.hbm2ddl.auto}") String hbm2ddl,
-            @Value("${hibernate.dialect}") String dialect) {
+    public Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.put("hibernate.show_sql", showSql);
-        properties.put("hibernate.hbm2ddl.auto", hbm2ddl);
-        properties.put("hibernate.dialect", dialect);
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
         return properties;
     }
 
     @Bean
-    public SessionFactory sessionFactory(DataSource dataSource,
-                                         @Value("${hibernate.packagesToScan}") String packagesToScan,
-                                         Properties hibernateProperties
-    ) throws IOException {
+    public SessionFactory sessionFactory(DataSource dataSource) throws IOException {
         LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setPackagesToScan(packagesToScan);
+        sessionFactoryBean.setPackagesToScan("lv.restaurant.core.domain");
         sessionFactoryBean.setDataSource(dataSource);
-        sessionFactoryBean.setHibernateProperties(hibernateProperties);
+        sessionFactoryBean.setHibernateProperties(hibernateProperties());
         sessionFactoryBean.afterPropertiesSet();
         return sessionFactoryBean.getObject();
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
-        return new HibernateTransactionManager(sessionFactory);
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
     }
+
 
 }
